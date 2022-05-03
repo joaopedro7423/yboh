1b0ae99f3a43ad5b3ac60a28ed41489f17aa7feb
Joao Pedro Moreira Palma <joaopedro7423@hotmail.com>
2022-05-02 21:56:31 -0400

    feat: create read file and push to database

--- src/router.ts
+++ src/router.ts
@@ -1,17 +1,62 @@
 import { Request, Response, Router } from "express";
-
+import { Readable } from "stream";
 import multer from "multer";
+import readline from "readline";
+import { client } from "./database/client";
 
 const multerConfit = multer();
 
 const router = Router();
 
+interface Users {
+  id: string;
+  name: string;
+  jobArea: string;
+  createdAts?: string;
+  deletedAts?: any;
+}
+
 router.post(
   "/arquivo",
   multerConfit.single("file"),
-  (request: Request, response: Response) => {
-    console.log(request.file?.buffer.toString("utf-8"));
-    return response.send();
+  async (request: Request, response: Response) => {
+    const { file } = request;
+    const buffer = file?.buffer;
+
+    const readableFile = new Readable();
+    readableFile.push(buffer);
+    readableFile.push(null);
+
+    const userLine = readline.createInterface({
+      input: readableFile,
+    });
+
+    const users: Users[] = [];
+
+    for await (let line of userLine) {
+      const lineSplit = line.split(";");
+      users.push({
+        id: lineSplit[0],
+        name: lineSplit[1],
+        jobArea: lineSplit[2],
+        createdAts: lineSplit[3] as string,
+        deletedAts: lineSplit[4] as string,
+      });
+    }
+
+    for await (let user of users) {
+      await client.users.create({
+        data: {
+          id: user.id,
+          name: user.name,
+          jobArea: user.jobArea,
+          cratedAts: user.createdAts,
+          deletedAts: user.deletedAts,
+        },
+      });
+    }
+
+    return response.json(users);
   }
 );
 
